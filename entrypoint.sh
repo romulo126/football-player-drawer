6c78fd6b70d35146aa032d076328b219612facb3
root <romulohenrique126@hotmail.com>
2024-05-07 16:10:33 +0000

    modify endpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "Iniciando a instalação das dependências do Composer..."
 cd /var/www/html && composer install
 
-echo "Copiando o .env.example para .env
+echo "Copiando o .env.example para .env"
 cp /var/www/html/.env.example /var/www/html/.env
 
 echo "Executando teste do Laravel..."
