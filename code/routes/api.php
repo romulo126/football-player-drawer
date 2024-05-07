dfbd0958fe1d0d757e7ad50b484c49fca61e46b9
romulo <romulo.henrique@atlastechnol.com>
2024-05-07 11:17:39 -0300

    feat: add new routes

--- code/routes/api.php
+++ code/routes/api.php
@@ -11,19 +11,29 @@ Route::group([
         'prefix' => 'player',
         'namespace' => 'Player',
     ], function () {
+        Route::get('/', 'SoccerPlayerAllController');
         Route::post('store', 'StoreSoccerPlayerController');
         Route::post('update/{id}', 'UpdateSoccerPlayerController');
         Route::delete('delete/{id}', 'DeleteSoccerPlayerController');
+
+        Route::group([
+            'prefix' => 'search'
+        ], function () {
+            Route::get('confirmed', 'SoccerPlayerConfirmedController');
+            Route::get('not/confirmed', 'SoccerPlayerNotConfirmedController');
+        });
+        
     });
 
     Route::group([
         'prefix' => 'team',
         'namespace' => 'Team',
     ], function () {
+        Route::get('/', 'AllSoccerTeamController');
         Route::post('store', 'StoreSoccerTeamController');
         Route::post('update/{id}', 'UpdateSoccerTeamController');
         Route::delete('delete/{id}', 'DeleteSoccerTeamController');
-        Route::get('/', 'AllSoccerTeamController');
+        Route::get('last/draw', 'LastDrawSoccerTeamController');
         Route::get('draw', 'DrawSoccerTeamController');
     });
 });
